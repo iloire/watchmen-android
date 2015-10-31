9d2ffebfeffd175862505e477d30e0985f4a754d
Ivan Loire <iloire@atlassian.com>
2015-10-31 20:36:54 +1100

    fix: fix server URL

--- app/src/main/java/com/iloire/watchmen/Settings.java
+++ app/src/main/java/com/iloire/watchmen/Settings.java
@@ -1,10 +1,7 @@
 package com.iloire.watchmen;
 
 public class Settings {
-
-    // TODO: move to resources
-    private static final String API_BASE_URL = "http://ec2-54-204-149-175.compute-1.amazonaws.com:3334";
-
+    private static final String API_BASE_URL = "http://watchmen.letsnode.com";
     public static String getServiceBaseUrl () {
         return API_BASE_URL;
     }
